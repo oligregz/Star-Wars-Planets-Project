47efda45293344caa5cda55a1460c3dd70a7ee02
gregory <gregorysak90@gmail.com>
2022-09-26 21:15:26 -0300

    Updates

---
+++ precache-manifest.471285656ce1e20b517c0f7ace006245.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f675f13ec905ea230f08ac8b207a9619",
+    "url": "/Star-Wars-Planets-Project/index.html"
+  },
+  {
+    "revision": "43359b0dfd4812d7e804",
+    "url": "/Star-Wars-Planets-Project/static/css/main.e3e5c6ae.chunk.css"
+  },
+  {
+    "revision": "8c29f7233e50cf864fb3",
+    "url": "/Star-Wars-Planets-Project/static/js/2.e7206b52.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Star-Wars-Planets-Project/static/js/2.e7206b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43359b0dfd4812d7e804",
+    "url": "/Star-Wars-Planets-Project/static/js/main.0c61738c.chunk.js"
+  },
+  {
+    "revision": "65761f07afb2b25987d8",
+    "url": "/Star-Wars-Planets-Project/static/js/runtime-main.25c081be.js"
+  },
+  {
+    "revision": "9654c55b546ef47d789beb35d322426c",
+    "url": "/Star-Wars-Planets-Project/static/media/blue-red-universe.9654c55b.jpg"
+  }
+]);
